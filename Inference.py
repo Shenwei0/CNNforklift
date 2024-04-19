3314c704921cdeb58a3419aa8e2ac4f6d19cdcb3
Raptor <CG22VY@student.aau.dk>
2024-04-19 18:32:12 +0200

    Commit

--- Inference.py
+++ Inference.py
@@ -30,29 +30,4 @@ predicted_label = torch.argmax(output).item() # Hent predicted class label
 print(output)
 print("Predicted Label:", predicted_label)
 
-
-'''
-class_labels = {
-    0: 'class_0_label',
-    1: 'class_1_label',
-    # Add more labels as needed
-}
-
-for image_path in test_image_paths:
-    test_image = Image.open(image_path).convert('RGB')
-    test_image = transform(test_image)
-    test_image = test_image.unsqueeze(0)
-    
-    # Perform inference
-    with torch.no_grad():
-        output = model(test_image)
-    
-    # Get the predicted class label
-    predicted_label = torch.argmax(output).item()
-    
-    # Get the corresponding label from the dictionary
-    predicted_label_name = class_labels[predicted_label]
-    
-    # Print the predicted label
-    print(f"Predicted Label for {image_path}: {predicted_label_name}")
-'''
+#lol
