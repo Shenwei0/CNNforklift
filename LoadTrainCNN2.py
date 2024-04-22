e85c3014e493411e6bf3479f9fb5dc6652630c37
Raptor <CG22VY@student.aau.dk>
2024-04-22 14:17:57 +0200

    Commit

--- LoadTrainCNN2.py
+++ LoadTrainCNN2.py
@@ -28,15 +28,44 @@ class ImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, x):
         imgName = os.path.join(self.rootDir, self.imagePaths[x])
         image = Image.open(imgName).convert('RGB')
-        image = Image.resize(self.targetSize, Image.BILINEAR)
+        image = image.resize(self.targetSize, Image.BILINEAR)
 
         if self.transform:
             image = self.transform(image)
         
-        '''
-        Indsæt labelling herind
-        '''
+        #label = 1
+        if x < len(self.imagePaths)//3:
+            label = 0
+        elif x < len(self.imagePaths) and x > len(self.imagePaths)//3:
+            label = 1
+        else: 
+            label = 2
 
+        print(image)        
+
+        return image, label
+
+
+
+transform = transforms.Compose([transforms.Resize((256,256)),transforms.ToTensor()])
+
+Dataset = ImageDataset(rootDir='DATA_jet_car_ship', transform=transform)
+
+trainLoader = DataLoader(Dataset, batch_size=1, shuffle=True)
+
+# for x, (images, labels) in enumerate(trainLoader):
+#     for i in range(len(images)):
+#         imgName = Dataset.imagePaths[x * trainLoader.batch_size + 1]
+#         label = labels[i].item()
+#         print("Image name: ", imgName, " and label: ", label)
+
+
+
+# for batch_idx, (images, labels) in enumerate(trainLoader):
+#     for i in range(len(images)):
+#         imgName = Dataset.imagePaths[batch_idx * trainLoader.batch_size + i]
+#         label = labels[i].item()
+#         print("Image Name:", imgName, "| Label:", label)
 
 
 
